b9d116ccce5dce2af33d7ff429ccb6870f955096
Michael Madden <mikemadden42@icloud.com>
2022-05-22 00:00:34 -0500

    Add some initial activities.

---
+++ bored/activity.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def random_activity():
+    req = requests.get("https://www.boredapi.com/api/activity/")
+    data = req.json()
+    print(data["activity"])
+
+
+def activity_by_type(activity_type: str):
+    req = requests.get(f"https://www.boredapi.com/api/activity/?type={activity_type}")
+    data = req.json()
+    print(data["activity"])
+
+
+def activity_by_participants(activity_participants: int):
+    req = requests.get(
+        f"https://www.boredapi.com/api/activity/?participants={activity_participants}"
+    )
+    data = req.json()
+    print(data["activity"])
+
+
+if __name__ == "__main__":
+    random_activity()
+    activity_by_type("cooking")
+    activity_by_participants(4)
