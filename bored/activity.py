bcb9bed5a751cc42b294f8429505ce590d2d3d1f
Michael Madden <mikemadden42@icloud.com>
2023-03-16 19:26:54 -0500

    Add support for ruff.

--- bored/activity.py
+++ bored/activity.py
@@ -2,20 +2,23 @@ import requests
 
 
 def random_activity():
-    req = requests.get("https://www.boredapi.com/api/activity/")
+    req = requests.get("https://www.boredapi.com/api/activity/", timeout=10)
     data = req.json()
     print(data["activity"])
 
 
 def activity_by_type(activity_type: str):
-    req = requests.get(f"https://www.boredapi.com/api/activity/?type={activity_type}")
+    req = requests.get(
+        f"https://www.boredapi.com/api/activity/?type={activity_type}", timeout=10
+    )
     data = req.json()
     print(data["activity"])
 
 
 def activity_by_participants(activity_participants: int):
     req = requests.get(
-        f"https://www.boredapi.com/api/activity/?participants={activity_participants}"
+        f"https://www.boredapi.com/api/activity/?participants={activity_participants}",
+        timeout=10,
     )
     data = req.json()
     print(data["activity"])
